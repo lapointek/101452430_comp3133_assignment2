412d146cbdfdacdbcbdd374df4bd50fabd46b246
lapointek <156107790+lapointek@users.noreply.github.com>
2025-04-06 15:19:28 -0400

    added employee list component

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  styleUrls: ['./employee-list.component.scss'],
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+  employees: any[] = [];
 
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.loadEmployees();
+  }
+
+  loadEmployees() {
+    const GET_EMPLOYEES = gql`
+      query {
+        employees {
+          id
+          name
+          position
+        }
+      }
+    `;
+
+    this.apollo
+      .watchQuery({
+        query: GET_EMPLOYEES,
+      })
+      .valueChanges.subscribe(({ data }: any) => {
+        this.employees = data.employees;
+      });
+  }
 }
